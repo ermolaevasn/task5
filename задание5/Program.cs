8f1ea98667d28f0264cfb162ae2e6afb26acd0d5
Дом <@DESKTOP-7GF1H2I>
2017-07-13 16:31:50 +0500

    готово

--- задание5/Program.cs
+++ задание5/Program.cs
@@ -8,8 +8,40 @@ namespace задание5
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main(string[] args)        
         {
+            int[,] mas = new int[10, 10];//создание матрицы размером 10
+            int i=0,j=0,max=0;
+            Random r = new Random();
+            for (int m = 0; m < 10; m++) //заполнение матрицы
+                for (int n = 0; n < 10; n++)
+                {
+                    mas[m, n] = r.Next(-50, 50);
+                }
+            for (int m = 0; m < 10; m++)// вывод матрицы на экран
+            {
+                for (int n = 0; n < 10; n++)
+                {
+                    Console.Write("{0,5}", mas[m, n]);
+                }
+                Console.WriteLine();
+            }
+            do
+            {
+                if (mas[i,j]<0) //проверка на отрицательный элемент в главной диагонали
+                {
+                    max = mas[i, 0];
+                    for (int k = 1; k < 10; k++)
+                    {
+                        if (mas[i, k] > max) max = mas[i, k]; // нахождение максимума
+                    }
+                    Console.WriteLine("В "+ (i+1)+ " строке максимальный элемент = "+ max);//вывод результата
+                }
+             i++;
+             j++;
+            } while (i<10);
+
+            Console.ReadKey();       
         }
     }
 }
